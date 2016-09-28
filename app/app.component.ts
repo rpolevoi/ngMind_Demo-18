d75f4b33aeaddf9c02a1d70a3396ea1711788f1c
Robert Polevoi <robert.polevoi@gmail.com>
2016-09-27 17:14:21 -0700

    Update app.component.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,17 +1,31 @@
 import { Component } from '@angular/core';
-import { StoreService } from '../store.service';
+import { ChildComponent } from './child/child.component';
+import { StoreService } from './store.service';
+import 'rxjs/add/operator/do';
+
 
 @Component({
   moduleId: module.id,
-  selector: 'app-child-component',
-  template: `<h3>Shared Store displayed in child = {{store.sub | async}}</h3>
-             <button (click)="incrementStore()" >Click to ++ store value</button>`,
-  styleUrls: ['child.component.css']
+  selector: 'root',
+  template: `<h3>Shared Store displayed in parent = {{storeVal}}</h3>
+              <h3 *ngIf="lastVal >= 0">Previous value was {{lastVal}}</h3>
+             <button (click)="incrementStore()" >Click to ++ store value</button>
+             <br>
+             <br>
+             <app-child-component></app-child-component>`,
+  styleUrls: ['app.component.css']
 })
-export class ChildComponent {
+export class AppComponent {
   
-  constructor(private store: StoreService) {}
+  storeVal: number;
+  lastVal: number;
   
-  incrementStore() {this.store.incrementValue()};
- 
+  constructor(private store: StoreService) {
+              this.store.sub
+                .do(() => this.lastVal = this.storeVal)
+                .subscribe(n => this.storeVal = n);
+  }
+  
+  incrementStore() {this.store.incrementValue();}
+  
 }
